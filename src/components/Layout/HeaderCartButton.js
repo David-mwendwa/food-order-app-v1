7fd6e9916b06a0a7eadf289dd047a2295b7a1aad
David Mwendwa <klatzdavie19@gmail.com>
2021-11-10 15:07:46 +0300

    fix: link the cart icon styles

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -6,7 +6,7 @@ import classes from './HeaderCartButton.module.css';
 const HeaderCartButton = (props) => {
   return (
     <button className={classes.button}>
-      <span>
+      <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
