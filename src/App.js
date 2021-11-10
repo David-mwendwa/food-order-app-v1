be524658eef97a1a5ffdcf9a74070d896baefbb1
David Mwendwa <klatzdavie19@gmail.com>
2021-11-10 14:54:24 +0300

    feat: add a _Header_ component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Fragment } from "react";
+
+import Header from "./components/Layout/Header";
+
 function App() {
   return (
-    <div>
-      <h2>mlo delicacies</h2>
-    </div>
+    <Fragment>
+      <Header/>
+    </Fragment>
   );
 }
 
