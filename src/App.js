a3430e1d1905e406a3d9ade2d0b4c011dc1a48bc
techdave <klatzdavie19@gmail.com>
2022-03-14 19:51:07 +0300

    v2 initial commit"

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import { Fragment, useState } from 'react';
-import Cart from './components/Cart/Cart';
-
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
-
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-  };
-
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
-    </CartProvider>
+    <div>
+      <h1>mlo Delicacies</h1>
+    </div>
   );
 }
 
